9c60cde727cc990878ef92e3b2c30a77324e2abc
Italo Alves <italo_silva007@hotmail.com>
2022-07-28 17:59:29 -0300

    style: add authentication page style

--- src/styles/Login/styles.ts
+++ src/styles/Login/styles.ts
@@ -1,153 +1,91 @@
-import styled from "styled-components";
+import styled from 'styled-components'
+import { Input as StyledInput } from '../../components/Form/Input'
 
-export const Container = styled.main`
+export const Main = styled.main`
+  display: flex;
   flex: 1;
-`;
-export const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-
-  max-width: 1350px;
-  margin: 0 auto;
-
-  h1 {
-    padding-bottom: 1.2rem;
-    margin: 1.5rem 2rem 0 2rem;
-    border-bottom: 1px solid #3e4247;
-  }
-`;
-
-export const FormContainer = styled.div`
-  height: 100%;
-
-  display: flex;
-  flex-direction: column;
   justify-content: center;
+  align-items: center;
+`
 
-  form {
-    padding: 2rem 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-`;
-
-export const Column = styled.div`
+export const Container = styled.div`
   width: 100%;
-  max-width: 100%;
-  flex: 0 0 100%;
+  max-width: 500px;
 
-  @media (min-width: 768px) {
-    max-width: 41.66667%;
-    flex: 0 0 41.66667%;
+  h2 {
+    line-height: 3.6rem;
+    font-size: 3rem;
+    text-align: center;
   }
-`;
+`
 
 export const Card = styled.div`
-  position: relative;
-
-  display: flex;
-  flex-direction: column;
-
-  word-wrap: break-word;
+  width: 100%;
+  margin-top: 2.4rem;
 
   background: #ffffff;
-  background-clip: border-box;
 
   border: 1px solid rgba(0, 0, 0, 0.125);
-  border-radius: 5px;
+  border-radius: 0.4rem;
 
   box-shadow: 0 0 60px rgba(0, 0, 0, 0.05);
-`;
 
-export const TopContainer = styled.div`
+  padding: 3rem;
+`
+
+export const Form = styled.form`
   display: flex;
   flex-direction: column;
+  gap: 2.4rem;
+`
 
-  padding: 3rem 3rem 0;
-
-  > div {
-    margin: 14px 0 10px;
-  }
-`;
-
-export const GridContainer = styled.section`
-  display: grid;
-  grid-auto-flow: row;
-  gap: 10px;
-`;
-
-export const FormGroup = styled.div`
-  input {
-    display: block;
-
-    width: 100%;
-    height: 33.5px;
-
-    padding: 0.6rem 1.2rem;
-
-    font-size: 1.6rem;
-
-    color: #24292e;
-
-    border: 1px solid var(--color-input-border);
-    border-radius: 5px;
-
-    box-shadow: var(--color-shadow-inset);
-
-    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-
-    &:focus {
-      border-color: var(--color-state-focus-border);
-      outline: none;
-      box-shadow: var(--color-state-focus-shadow);
-    }
-  }
-
-  button {
-    width: 100%;
-
-    border-radius: 6px;
-    padding: 12px 16px;
-    background: var(--color-buttons);
-
-    text-transform: uppercase;
-    font-weight: bold;
-
-    transition: background-color 0.15s ease-in-out;
-
-    &:disabled {
-      background: rgba(36, 41, 46, 0.85);
-
-      color: rgba(255, 255, 255, 0.35);
-      cursor: not-allowed;
-    }
-
-    &:not(:disabled):hover {
-      background: var(--color-buttons-hover);
-    }
-  }
-`;
-
-export const BottomContainer = styled.div`
+export const BackContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 
-  padding: 0 3rem 3rem;
-`;
+  > div:first-child {
+    display: flex;
+    align-items: center;
+    gap: 0.8rem;
 
-export const Row = styled.div`
-  display: grid;
-  grid-auto-flow: row;
-  gap: 4px;
+    input {
+      width: 1.6rem;
+      height: 1.6rem;
+    }
 
-  a {
-    color: #000;
+    label {
+      font-size: 1.4rem;
+    }
+  }
+
+  > div:last-child {
     font-size: 1.4rem;
+    line-height: 2rem;
 
-    &:hover {
-      text-decoration: underline;
+    span {
+      color: black;
+
+      &:hover {
+        text-decoration: underline;
+      }
     }
   }
-`;
+`
+
+export const ButtonContainer = styled.div`
+  > p {
+    font-size: 1.4rem;
+    margin-top: 1rem;
+
+    span {
+      margin-left: 0.5rem;
+      font-size: 1.4rem;
+      color: #3b82f6;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+`
